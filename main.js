089356e9fd2dd4a870c69e931ee5ce30bbcafb99
Kaotisk Hund <kaotiskhund@gmail.com>
2017-11-09 05:15:27 +0200

    Don't need dev-tools for now

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ try {
 	  }))
 
 	  // Open the DevTools.
-	  win.webContents.openDevTools()
+	  // win.webContents.openDevTools()
 
 	  // Emitted when the window is closed.
 	  win.on('closed', () => {
