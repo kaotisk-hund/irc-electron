4bb81f91c5a266c34299ca03095d3a687aaeac05
Kaotisk Hund <kaotiskhund@gmail.com>
2017-11-09 04:39:26 +0200

    Main program

---
+++ main.js
@@ -0,0 +1,187 @@
+const {app, BrowserWindow, ipcMain, Menu} = require('electron')
+const url = require('url')
+const path = require('path')
+const irc = require('irc')
+process.env.NODE_ENV = 'production'
+let client
+let channel
+let message
+let tmp
+let win
+
+try {
+	function createWindow () {
+	  // Create the browser window.
+	  win = new BrowserWindow({width: 800, height: 600})
+
+	  // and load the index.html of the app.
+	  win.loadURL(url.format({
+	    pathname: path.join(__dirname, 'index.html'),
+	    protocol: 'file:',
+	    slashes: true
+	  }))
+
+	  // Open the DevTools.
+	  win.webContents.openDevTools()
+
+	  // Emitted when the window is closed.
+	  win.on('closed', () => {
+	    // Dereference the window object, usually you would store windows
+	    // in an array if your app supports multi windows, this is the time
+	    // when you should delete the corresponding element.
+	    win = null
+	  })
+
+	  // Build menu from template
+	  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	  // Insert menu
+	  Menu.setApplicationMenu(mainMenu);
+	}
+
+
+// Send message
+ipcMain.on('irc_send', function(e, message){
+	tmp.say(channel,message)
+})
+
+// Catch item:add
+ipcMain.on('irc_connect', function(e, thadata){
+  //win.webContents.send('irc:connect', thadata);
+  client = null
+  var server = thadata.server;
+  var nickname = thadata.nickname;
+  channel = thadata.channel;
+  const username = nickname+'_kirc';
+  const realname = nickname+' at KiRc';
+ 
+  console.log(server)
+  console.log(nickname)
+  console.log(channel)
+  tmp = new irc.Client(server, nickname, {
+	channels: [
+		channel
+	],
+	userName: username,
+	realName: realname
+  });
+  
+  tmp.join(channel);
+  tmp.addListener('message'+channel, function (from, message) {
+  	var data = {from, message}
+    win.webContents.send('irc_message', data);
+    console.log(from + ': ' + message);
+    //tmp.say(channel, 'goystareis?');
+
+  });
+  
+  //addWindow.close(); 
+  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
+  //addWindow = null;
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+} catch (e){
+  console.log(e.message);
+}
+
+
+// Create menu template
+const mainMenuTemplate =  [
+  // Each object is a dropdown
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label:'Clear Items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+  	label: 'Options',
+  	submenu:[
+  		{
+  			label:'Connection settings'
+  		},
+  		{
+  			label:'Connect'
+  		},
+  		{
+  			label:'Disconnect'
+  		}
+  	]
+  },
+  {
+    label: 'Help',
+    submenu:[
+      {
+        label: 'About',
+        click(){
+          aboutWindow();
+        }
+      }
+    ]
+  }
+];
+
+// If OSX, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
