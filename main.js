e1ad8b299e1a8d91c0759a0292429db4cf45a4be
Kaotisk Hund <kaotiskhund@gmail.com>
2017-11-18 05:56:00 +0200

    Pretty spaces

--- main.js
+++ main.js
@@ -148,74 +148,74 @@ try {
 
 	// Connect Function
 	function connect(e, thadata, client){
-  //win.webContents.send('irc:connect', thadata);
-  console.log(e)
-  server = thadata.server;
-  nickname = thadata.nickname;
-  channel = thadata.channel;
-  username = nickname+'_kirc';
-  realname = nickname+' at KiRc';
-  
-  console.log('=========   Your login info   =========')
-  console.log(server)
-  console.log(nickname)
-  console.log(channel)
+	//win.webContents.send('irc:connect', thadata);
+	console.log(e)
+	server = thadata.server;
+	nickname = thadata.nickname;
+	channel = thadata.channel;
+	username = nickname+'_kirc';
+	realname = nickname+' at KiRc';
+	
+	console.log('=========	 Your login info	 =========')
+	console.log(server)
+	console.log(nickname)
+	console.log(channel)
 
 
-  console.log('.............')
+	console.log('.............')
 
-   if (client === null){
-   	client = new irc.Client(server, nickname, {
+	 if (client === null){
+	 	client = new irc.Client(server, nickname, {
 		channels: [
 			channel
 		],
 		userName: username,
 		realName: realname
-	  });
-   	
-    client.addListener('registered', function(mess){
-      console.log('CDed');
-      console.log('Nickname: ' + client.nick);
-      console.log(mess);
-      win.webContents.send('irc_cded');
-      client.join(channel);
-      setListeners(client);
-    })
-   } else {
-   	//client = null
-   	console.log('Now client is not set null BUT nothing else happens :D')
-   }
-   console.log('.............')
-}
+		});
+	 	
+		client.addListener('registered', function(mess){
+			console.log('CDed');
+			console.log('Nickname: ' + client.nick);
+			console.log(mess);
+			win.webContents.send('irc_cded');
+			client.join(channel);
+			setListeners(client);
+		})
+	 } else {
+	 	//client = null
+	 	console.log('Now client is not set null BUT nothing else happens :D')
+	 }
+	 console.log('.............')
+	}
 
-function disconnect(client){
-	client.disconnect();
-  client = null;
-}
+	function disconnect(client){
+		client.disconnect();
+		client = null;
+	}
 
-function setListeners(client){
-  client.addListener('message'+channel, function (from, message) {
-    data = {from, message};
-    // TODO add the function that adds the message
-    addMessageToBoard(data)
-  });
+	function setListeners(client){
+		client.addListener('message'+channel, function (from, message) {
+			data = {from, message};
+			// TODO add the function that adds the message
+			addMessageToBoard(data)
+		});
 
-  ipcMain.on('irc_send', function(e, data){
-    message = data;
-    if (client === null) {
-      console.log('wtf??? Maybe disconnected... Most likely.')
-    } else {
-      console.log('Seems you are online... going to send that message...')
-      client.say(channel,message)
-    }
-    from = nickname
-    data = {from, message};
-    // TODO add the function that adds the message
-    addMessageToBoard(data)
-    console.log(data)
-  })
+		ipcMain.on('irc_send', function(e, data){
+			message = data;
+			if (client === null) {
+				console.log('wtf??? Maybe disconnected... Most likely.')
+			} else {
+				console.log('Seems you are online... going to send that message...')
+				client.say(channel,message)
+			}
+			from = nickname
+			data = {from, message};
+			// TODO add the function that adds the message
+			addMessageToBoard(data)
+			console.log(data)
+		})
 
-}
+	}
 
 
 	// Catch irc_connect
