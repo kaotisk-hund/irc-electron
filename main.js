62636e8503b3df6cb2562fb3db1f23912e15ea2a
Kaotisk Hund <kaotiskhund@gmail.com>
2017-12-21 19:47:50 +0200

    Refixed how it should look

--- main.js
+++ main.js
@@ -151,7 +151,7 @@ if(process.env.NODE_ENV !== "production"){
  */
 function mainWindow () {
 	// Create the browser window.
-	win = new BrowserWindow({width: 800, height: 600});
+	win = new BrowserWindow({width: 800, height: 610});
 	
 	// and load the index.html of the app.
 	win.loadURL(url.format({
