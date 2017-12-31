a3755f98c68c75c5dba455b0f79f3baecc5641f2
Kaotisk Hund <kaotiskhund@gmail.com>
2017-12-31 02:18:21 +0200

    Optimize (?) a bit
    New message window dimensions.
    Div class send is now div id send.
    New window dimensions.

--- main.js
+++ main.js
@@ -183,7 +183,7 @@ if(process.env.NODE_ENV !== "production"){
  */
 function mainWindow () {
 	// Create the browser window.
-	win = new BrowserWindow({width: 800, height: 610});
+	win = new BrowserWindow({width: 800, height: 600});
 	
 	// and load the index.html of the app.
 	win.loadURL(url.format({
